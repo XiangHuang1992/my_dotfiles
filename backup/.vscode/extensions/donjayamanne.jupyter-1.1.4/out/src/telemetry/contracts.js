418edffc3d5b870a4c04c4e740004a58200c9a73
huangxiang <huangxiang1992@hotmail.com>
2019-07-25 23:08:13 +0800

    remove vscode

--- backup/.vscode/extensions/donjayamanne.jupyter-1.1.4/out/src/telemetry/contracts.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Jupyter;
-(function (Jupyter) {
-    Jupyter.Usage = 'JUPYTER';
-})(Jupyter = exports.Jupyter || (exports.Jupyter = {}));
-exports.EVENT_LOAD = 'EXT_LOAD';
-//# sourceMappingURL=contracts.js.map
--- backup/.vscode/extensions/fknop.vscode-npm-3.3.0/out/src/output.js
+++
@@ -1,4 +0,0 @@
-var vscode_1 = require('vscode');
-var outputChannel = vscode_1.window.createOutputChannel('npm');
-exports.outputChannel = outputChannel;
-//# sourceMappingURL=output.js.map
